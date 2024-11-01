5b62e7ad0809bef62f890d593cbcc50d80bc6083
kashish00208 <kgupta7827@gmail.com>
2024-11-01 18:47:27 +0530

    Generating responce the way i want

--- src/app/api/chat/route.js
+++ src/app/api/chat/route.js
@@ -1,24 +1,28 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { NextResponse } from "next/server";
 
-export async function POST(req,res){
-    try{
-        const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-        const model = genAI.getGenerativeModel({model:"gemini-1.5-flash"});
-        const data = await req.json() 
+export async function POST(req, res) {
+  try {
+    const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+    const model = genAI.getGenerativeModel({
+      model: "gemini-1.5-flash",
+      max_output_tokens: 50,
+    });
+    const data = await req.json();
 
-        const prompt = data.body
-        if (!prompt) {
-            return NextResponse.json({ error: "Prompt is required" }, { status: 400 });
-        }
-
-
-        const results = await model.generateContent(prompt);
-        const responce = results.response
-        const output = await responce.text();
-        return NextResponse.json({output:output})
+    const prompt = "Provide a short and concise answers of prompt . Also talk like human"+data.body;
+    if (!prompt) {
+      return NextResponse.json(
+        { error: "Prompt is required" },
+        { status: 400 }
+      );
     }
-    catch(error){
-        return NextResponse.json({message:"responce is not okay"})
-    }
+
+    const results = await model.generateContent(prompt);
+    const responce = results.response;
+    const output = await responce.text();
+    return NextResponse.json({ output: output });
+  } catch (error) {
+    return NextResponse.json({ message: "responce is not okay" });
+  }
 }
