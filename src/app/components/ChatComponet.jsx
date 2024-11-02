067c7dbfb4d1357b660d2852ea460ae3ad498afd
kashish00208 <kgupta7827@gmail.com>
2024-11-02 18:41:52 +0530

    Changes

--- src/app/components/ChatComponet.jsx
+++ src/app/components/ChatComponet.jsx
@@ -1,11 +1,10 @@
 "use client";
 import React, { useRef, useState } from "react";
 
-
 const ChatComponent = () => {
   const [prompt, setPrompt] = useState("");
   const [response, setResponse] = useState("");
-  const [message,setMessage] = useState([]);
+  const [message, setMessage] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
   const msgEnd = useRef(null);
@@ -42,26 +41,31 @@ const ChatComponent = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={prompt}
-          onChange={(e) => setPrompt(e.target.value)}
-          placeholder="Type your prompt here"
-          required
-        />
-        <button type="submit" disabled={loading}>
-          {loading ? "Generating..." : "Submit"}
-        </button>
-      </form>
-      {error && <div style={{ color: "red" }}>{error}</div>}
-      {response && (
-        <div>
-          <h3>Response:</h3>
-          <p className="text-black">{response}</p>
+    <div className= "w-screen h-screen bg-slate-500">
+      <div className="w-3/4 px-40 py-24">
+        <div className="">
+          <form onSubmit={handleSubmit} className="w-full bottom-0 fixed px-10 py-14 ">
+            <input
+              className="w-2/4 rounded-2xl"
+              type="text"
+              value={prompt}
+              onChange={(e) => setPrompt(e.target.value)}
+              placeholder="Type your prompt here"
+              required
+            />
+            <button type="submit" disabled={loading} className="ml-8">
+              {loading ? "Generating..." : "Submit"}
+            </button>
+          </form>
         </div>
-      )}
+        {error && <div style={{ color: "red" }}>{error}</div>}
+        {response && (
+          <div>
+            <h3>Response:</h3>
+            <p className="text-black">{response}</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@ import ChatComponent from "./components/ChatComponet";
 export default function Home() {
   return (
     <>
-    <h1 className="text-center text-3xl bold">ChatBot</h1>
     <ChatComponent/>
     </>
   );
