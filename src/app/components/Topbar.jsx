3eab5b24bac66df83b3eed041171457d72e985b9
kashish00208 <kgupta7827@gmail.com>
2024-11-20 13:56:44 +0530

    minor changes

--- src/app/components/Topbar.jsx
+++ src/app/components/Topbar.jsx
@@ -4,9 +4,15 @@ import Image from "next/image";
 const Topbar = () => {
   return (
     <div className="h-2/6 fixed mx-10 md:mx-16 my-8 ">
-      <div className="flex justify-between"> 
+      <div className="flex justify-between">
         <h1 className="text-3xl font-mono">HelperBot</h1>
-        <Image src="/icons/home.svg" width={40} height={40} alt="HomeImage" className="ml-96" />
+        <Image
+          src="/icons/home.svg"
+          width={40}
+          height={40}
+          alt="HomeImage"
+          className="ml-96"
+        />
       </div>
     </div>
   );
